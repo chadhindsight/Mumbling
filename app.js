6c2a5244eec1d46f1c8457e5a0bf409c263027b4
Chad Hinds <chadrickj8@gmail.com>
2020-01-07 22:16:09 -0500

    Test if functions works by logging result to console

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const accum = (s) => {
-    return s.split('').map((x, index) => x.toUpperCase() + Array(index + 1).join(x.toLowerCase())).join('-');
+    // split array into a string first and save as a variable
+    let arr = s.split('');
+    return arr.map((x, index) => x.toUpperCase() + Array(index + 1).join(x.toLowerCase())).join('-');
 }
+
+console.log(accum('bait'));
