93010f8c17c57e889dfcccfe5109a7a9c6221000
Chad Hinds <chadrickj8@gmail.com>
2020-01-13 20:34:11 -0500

    Comment on how the split method is being used

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const accum = (s) => {
-    // split array into a string first and save as a variable
+    //Split array into a string first and save that string as a variable
     let arr = s.split('');
+    
     return arr.map((x, index) => x.toUpperCase() + Array(index + 1).join(x.toLowerCase())).join('-');
 }
 
