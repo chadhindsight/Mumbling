11afca1cba97c693912a36dc6a14af169b45e64d
Chad Hinds <chadrickj8@gmail.com>
2020-01-28 10:02:04 -0500

    Alter the name of the argument passed into map callback

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const accum = (s) => {
     //Split array into a string first and save that string value as a variable
     let arr = s.split('');
     
-    return arr.map((x, index) => x.toUpperCase() + Array(index + 1).join(x.toLowerCase())).join('-');
+    return arr.map((y, index) => y.toUpperCase() + Array(index + 1).join(y.toLowerCase())).join('-');
 }
 
 console.log(accum('stadium'));
